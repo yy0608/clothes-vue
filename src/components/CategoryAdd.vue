b3686f8fd982cb9a1d20ac622523975a65b4be01
yy0608 <447462224@qq.com>
2018-03-28 17:57:03 +0800

    完成分类添加功能

--- src/components/CategoryAdd.vue
+++ src/components/CategoryAdd.vue
@@ -1,18 +1,24 @@
 <template>
-<div class="manage-item merchants-manage-cont">
+<div class="manage-item category-add-manage-cont">
   <title-cont :title="'分类列表 > 添加分类'" :back="true"></title-cont>
   <el-form class="merchant-add-form" :model="form" ref="form" label-width="80px" label-position="left" :rules="rules">
-    <el-form-item label="名称" prop="value">
-      <el-input v-model="form.value"></el-input>
+    <el-form-item label="名称" prop="label">
+      <el-input v-model="form.label"></el-input>
     </el-form-item>
-    <el-form-item label="上级分类" prop="id">
+    <el-form-item label="上级分类" prop="parent">
       <el-cascader
-        :options="options"
+        clearable
         change-on-select
+        v-model="form.parent"
+        @change="handleChange"
+        :options="goodsCategories"
       ></el-cascader>
     </el-form-item>
-    <el-form-item label="logo" prop="logo">
-      <el-input v-model="form.logo"></el-input>
+    <el-form-item v-if="iconInputShow" label="图标" prop="icon">
+      <el-input v-model="form.icon"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submit">添加</el-button>
     </el-form-item>
   </el-form>
 </div>
@@ -23,31 +29,20 @@ import axios from 'axios'
 import { origin } from '@/config.js'
 import TitleCont from './TitleCont.vue'
 import Upload from './Upload.vue'
+import { generateGuid } from '@/utils.js'
 
 export default {
   data () {
     return {
-      // options: [{
-      //   label: 'ccc',
-      //   value: 'aaa',
-      //   children: [{
-      //     label: '111',
-      //     value: 'sss',
-      //     children: [{
-      //       label: 'ggg',
-      //       logo: 'http://www.sss.com/',
-      //       value: '222'
-      //     }]
-      //   }]
-      // }]
-      options: [],
+      goodsCategories: [],
       form: {
-        value: '',
-        id: '',
-        logo: ''
+        label: '',
+        parent: [],
+        icon: ''
       },
+      iconInputShow: false,
       rules: {
-        value: [{
+        label: [{
           required: true,
           message: '输入分类名称'
         }]
@@ -58,6 +53,9 @@ export default {
     TitleCont,
     Upload
   },
+  created () {
+    this.getGoodsCategories()
+  },
   methods: {
     getGoodsCategories () {
       axios({
@@ -66,19 +64,98 @@ export default {
         method: 'get'
       })
         .then(res => {
-          console.log(res.data)
+          console.log(111, res.data.data)
+          if (!res.data.success) {
+            return this.$message.error('获取分类列表失败')
+          }
+          this.goodsCategories = res.data.data
         })
         .catch(err => {
           console.log(err)
         })
+    },
+    handleChange (value) {
+      // this.form.parent = value
+      switch (value.length) {
+        case 0:
+          this.iconInputShow = false
+          this.rules = {
+            label: [{
+              required: true,
+              message: '输入分类名称'
+            }]
+          }
+          break
+        case 1:
+          this.iconInputShow = false
+          this.rules = {
+            label: [{
+              required: true,
+              message: '输入分类名称'
+            }]
+          }
+          break
+        case 2:
+          this.iconInputShow = true
+          this.rules = {
+            label: [{
+              required: true,
+              message: '输入分类名称'
+            }],
+            icon: [{
+              required: true,
+              message: '上传icon或输入地址'
+            }]
+          }
+          break
+      }
+      console.log(this.form.parent)
+    },
+    submit () {
+      this.$refs.form.validate(bool => {
+        if (!bool) {
+          console.log(this.form)
+          this.$message('填写出错')
+          return
+        }
+        axios({
+          url: origin + '/employ/category_add',
+          withCredentials: true,
+          method: 'post',
+          data: {
+            value: generateGuid(),
+            parent: this.form.parent,
+            label: this.form.label,
+            icon: this.form.icon
+          }
+        })
+          .then(res => {
+            if (res.data.success) {
+              this.getGoodsCategories()
+              this.$refs.form.resetFields()
+              this.$message.success(res.data.msg)
+            } else {
+              this.$message.error(res.data.msg)
+            }
+          })
+          .catch(err => {
+            console.log(err)
+            this.$message.error('请求出错')
+          })
+      })
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.el-form {
-  padding: 20px;
-  width: 500px;
+<style lang="scss">
+.category-add-manage-cont {
+  .el-form {
+    padding: 20px;
+    width: 500px;
+    .el-cascader {
+      width: 100%;
+    }
+  }
 }
 </style>
